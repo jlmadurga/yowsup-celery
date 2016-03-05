492a1b8ade0c96e26d5d1f7154d20f6bc06bbc6f
Juan Madurga <jlmadurga@gmail.com>
2016-03-05 11:11:26 +0100

    Bump version: 0.1.5 → 0.2.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = requirements + [
 
 setup(
     name='yowsup-celery',
-    version='0.1.5',
+    version='0.2.0',
     description="Yowsup integrated in a celery architecture",
     long_description=readme + '\n\n' + history,
     author="Juan Madurga",
--- yowsup_celery/__init__.py
+++ yowsup_celery/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Juan Madurga'
 __email__ = 'jlmadurga@gmail.com'
-__version__ = '0.1.5'
+__version__ = '0.2.0'
